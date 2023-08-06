5c1096e29ba8d70dd60356fb94b26212488e1851
ilinti1 <141251460+ilinti1@users.noreply.github.com>
2023-08-06 18:58:30 +0300

    Add files via upload

---
+++ up.js
@@ -0,0 +1,20 @@
+const slides = document.querySelectorAll(' .slide' )
+
+for (const slide of slides) {
+    slide.addEventListener('click', ()=>{
+        clearActiveClasses()
+
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide)=> {
+slide.classList.remove('active')
+    })
+}
+
+let Hi = prompt('сколько тебе лет?', '')
+ if (Hi == 19 ) {alert( 'Ты лапочка!' );}
+ else {alert( 'Buy' );}
+ 
